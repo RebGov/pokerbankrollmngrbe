79e25ed34632e35a1e210536a0cad5babdb7349c
RebGov <becci.govert@gmail.com>
2018-11-05 15:02:18 -0600

    changes in auth and users

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -27,7 +27,7 @@ class Api::V1::UsersController < ApplicationController
 
   private
   def user_params
-    params.permit(:id, :email, :password, :first_name, :last_name, :username, notes:[])
+    params.permit(:id, :email, :password, :first_name, :last_name, notes:[])
   end
 
 end
