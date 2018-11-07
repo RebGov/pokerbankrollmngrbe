edb13b1a6a7732e5782594c35025ee514eecf6ba
RebGov <becci.govert@gmail.com>
2018-11-06 19:42:04 -0600

    added note controller and routes

--- app/controllers/api/v1/notes_controller.rb
+++ app/controllers/api/v1/notes_controller.rb
@@ -1,2 +1,28 @@
 class Api::V1::NotesController < ApplicationController
+  skip_before_action :check_authentication
+  def create
+    note = Note.create(note_params)
+    if gameName.valid?
+      render json: { note: note }
+    else
+      render json: { errors: note.errors}
+    end
+  end
+
+  def index
+    notes = Note.all
+    render json: { notes: notes }
+  end
+
+  def update
+  end
+
+  def delete
+  end
+
+  private
+
+  def note_params
+    params.permit(:id, :note, :played_game_id)
+  end
 end
